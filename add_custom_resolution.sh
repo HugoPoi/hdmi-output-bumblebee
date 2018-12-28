c8e279183ecb71017d5cb84c7060392018a6be10
HugoPoi <hugo@poissonnet.fr>
2018-12-28 20:07:46 +0100

    Add custom resolution script needed for some display, especially mirroring

---
+++ add_custom_resolution.sh
@@ -0,0 +1,3 @@
+sudo xrandr --newmode "1280x720_60.00"   74.50  1280 1344 1472 1664  720 723 728 748 -hsync +vsync
+sudo xrandr --addmode VIRTUAL1 "1280x720_60.00"
+sudo xrandr --addmode VIRTUAL2 "1280x720_60.00"
